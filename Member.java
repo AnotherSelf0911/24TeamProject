a3b819d8802c1c543797cb22e896f7435df4eda9
lunatic1317 <151497792+lunatic1317@users.noreply.github.com>
2023-11-28 16:04:39 +0900

    Add files via upload

--- Member.java
+++ Member.java
@@ -1,25 +1,22 @@
-package teamProject;
-
+package exercise01;
 import java.util.Date;
 
-public class Member {
+public class Member{
 	
-	int memberAge;		// 나이
-	float memberWeight; // 몸무게
-	float memberLength; // 키
-	float memberBMI; // BMI 수치
-	String memberName;	// 이름
-	String memberGender;// 성별
-	Date startDate;		// 시작일 (회원가입일)
-	int days;			// 회원가입일로부터 몇일 지났는지
-	int exerciseDays;	// 운동한 날짜
-	String memberGrade;	// 회원 등급
-	String memberExerciseDiff;	// 회원 운동 강도
-	String memberID;	// 회원 아이디
-	String memberPW;	// 회원 비밀번호
+	String name;
+	float weight;
+	float Startweight;
+	float Length;
+	String gender;
+	int age;
+	int exerciseDays;
+	Date startDate;
 	
-	public String getMemberID() {
-		return memberID;
+	
+	public Member(String name, Date startDate) {
+		this.name = name;
+		this.startDate = startDate;
 	}
+	
 	
 }
