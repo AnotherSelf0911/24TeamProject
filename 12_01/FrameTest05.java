0c27036db5f356f662947b089ab1b279093745ea
AnotherSelf0911 <152248730+AnotherSelf0911@users.noreply.github.com>
2023-12-01 15:12:44 +0900

    Add files via upload

--- 12_01/FrameTest05.java
+++ 12_01/FrameTest05.java
@@ -1,6 +1,7 @@
 package frame_pro;
 
 import java.awt.*;
+import java.awt.event.*;
 
 public class FrameTest05 {
 
@@ -10,7 +11,15 @@ public class FrameTest05 {
 		fr.setBackground(Color.yellow);
 		fr.setTitle("다섯번째 사용자 프레임");
 		
-		fr.addWindowListener(new MyEventListener());
+		// #1 fr.addWindowListener(new MyEventListener());
+		// #2 fr.addWindowListener(new WinClosingListener());
+		// #3 익명 내부 클래스로 바로 어댑터 생성
+		fr.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.out.println("어댑터 생성하여 감지!!");
+			}
+		});
 	}
 
 }
